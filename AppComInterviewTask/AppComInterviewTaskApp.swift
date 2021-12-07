d9e8cd29d579f126fd2b49eb291c108e902aa725
Thahiraf <thahiraf@gmail.com>
2021-12-07 15:09:01 +0400

    Initial Commit

---
+++ AppComInterviewTask/AppComInterviewTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppComInterviewTaskApp.swift
+//  AppComInterviewTask
+//
+//  Created by Thahi on 07/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct AppComInterviewTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
